9fb693d83ec5bbb6344484f6be5c11dbb5c9fd7b
tsunose <tsunose.developer@gmail.com>
2024-01-20 17:58:14 +0900

    implements

--- src/app/app-router/page.tsx
+++ src/app/app-router/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next'
+
+// metaデータの上書き
+// only server component
+export const metadata: Metadata = {
+  title: 'app-router page',
+  metadataBase: new URL('https://acme.com'),
+}
+
 export default function Page() {
   return <h1>App Routerで動くページ</h1>
 }
