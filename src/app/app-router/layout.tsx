a410e762989990e416e2e1e51baad26ab660f899
tsunose <tsunose.developer@gmail.com>
2024-02-08 16:55:21 +0900

    implements

--- src/app/app-router/layout.tsx
+++ src/app/app-router/layout.tsx
@@ -1,10 +1,13 @@
 import AxiosProvider from '@/providers/AxiosProvider';
 import TanstackQueryProvider from '@/providers/TanstackQueryProvider';
+import JotaiProvider from '@/providers/JotaiProvider';
 
 export default function AppRouterLayout({ children }: { children: React.ReactNode }) {
   return (
     <AxiosProvider>
-      <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      <JotaiProvider>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </JotaiProvider>
     </AxiosProvider>
   );
 }
