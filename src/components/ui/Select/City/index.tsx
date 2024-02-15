e243b496e982a2ea4fae5dd1559ffb6dd18e3e8f
tsunose <tsunose.developer@gmail.com>
2024-02-15 15:40:39 +0900

    remove

--- src/components/ui/Select/City/index.tsx
+++ src/components/ui/Select/City/index.tsx
@@ -8,7 +8,6 @@ type CityProps = {
 
 export const City: FC<CityProps> = ({ cityList }) => {
   const items = useMemo(() => {
-    // TODO: forEachの方がいいと思っている（副作用）
     return cityList?.map((element, index) => {
       return (
         <SelectItem key={index + 1} value={element.id}>
